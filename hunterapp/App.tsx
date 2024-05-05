f00de72318c84f36fb046a23bf82f48f3853e04a
larissa <159553145+l4riss2@users.noreply.github.com>
2024-05-04 21:52:19 -0300

    gittttt0405
    git 0405 app

--- hunterapp/App.tsx
+++ hunterapp/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View,Text} from 'react-native'
-import {SignIn} from './src/screens/index'
+import {View,Text,TextInput} from 'react-native';
+import {SignIn} from './src/screens/index';
+
 export default function App(){
  return(
-    <SignIn/>
+    <SignIn></SignIn>
   );
 
-}
+} 
 
--- hunterapp/src/screens/index.tsx
+++ hunterapp/src/screens/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {View,Text} from 'react-native'
+import {View,Text,TextInput} from 'react-native';
+import {styles} from './styles';
+
 
 export function SignIn(){
  return(
     <View>
       <Text>Hello World</Text>
     </View>
+   
   );
-
 }
+
+<View style={styles.container}>
+      <TextInput></TextInput>
+</View>
