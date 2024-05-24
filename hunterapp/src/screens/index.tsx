4779ebc30d0cd266c061d1715ee48455f4c011d1
sorrisin <159553145+l4riss2@users.noreply.github.com>
2024-05-24 11:55:47 -0300

    gitttt
    gittttttt

--- hunterapp/src/screens/index.tsx
+++ hunterapp/src/screens/index.tsx
@@ -1,13 +1,27 @@
 import React ,{useState} from 'react';
 import {View,Text,TextInput,Image} from 'react-native';
 import {styles} from './styles';
-import IllustrationImg from '../../src/assets/illustrationImg.png'
+import IllustrationImg from '../assets/illustration.png'
+import { StatusBar } from 'expo-status-bar';
 
 export function SignIn(){
   const [text,setText] = useState ("Vc não digitou nada ainda");
   return(
-    <View style={styles.container}>
-       <Image source={} />
+    <View style={styles.container}> 
+     <StatusBar barStyle='light-content' backgroundColor = {"transparent"} translucent/>
+      <Image source={IllustrationImg} style={styles.image}  resizeMode = 'stretch'/>
+    
+    <View style = {styles.content}>
+      <Text style = {styles.title}>
+        Organize Suas {'\n'}
+        Jogatinas {'\n'}
+        facilmente
+      </Text>
+      <Text style = {styles.subtitle}>
+       Crie grupos para jogar seus games {'\n'}
+       favoritos com seusn amigos
+      </Text>
+    </View>
     </View>
   );
 }
